774f3a336291659dda4dc0fcf499cd63f82a2535
Derrick Carr <derrick.the.future@gmail.com>
2017-02-10 21:47:56 -0800

    Initial Commit – Late
    After hacking away at this for a few hours, I decided to check this into
    version control. So far, I have a functional counter and a way to
    associate the right coordinates with it. As I'm getting further along,
    I keep butting up against the limits of not doing this with a stricter
    OO process. So this is the current version. Next step is to refactor
    the icon methods as objects that hold their own coordinates.

---
+++ simple_counter.js
@@ -0,0 +1,78 @@
+var defaultIconSize = 25;
+
+var makeAddIcon = function(x, y, iconSize, iconColorArray) {
+  strokeWeight(1); //Reinitialize default from previous drawings
+
+  fill(iconColorArray[0], iconColorArray[1], iconColorArray[2]);
+  rect(x, y, iconSize, iconSize); //Icon container
+
+  strokeWeight(3);
+  line(x + iconSize * 0.25, y + iconSize * 0.5, x + iconSize * 0.75, y + iconSize * 0.5); // Horizontal line
+  line(x + iconSize * 0.5, y + iconSize * 0.25, x + iconSize * 0.5, y + iconSize * 0.75); // Vertical line
+};
+var makeSubtractionIcon = function(x, y, iconSize, iconColorArray) {
+  strokeWeight(1); //Reinitialize default from previous drawings
+
+  fill(iconColorArray[0], iconColorArray[1], iconColorArray[2]);
+  rect(x, y, iconSize, iconSize); //Icon container
+
+  strokeWeight(3);
+  line(x + iconSize * 0.25, y + iconSize * 0.5, x + iconSize * 0.75, y + iconSize * 0.5); // Horizontal line
+};
+var makeEqualIcon = function(x, y, iconSize, iconColorArray) {
+  strokeWeight(1); //Reinitialize default from previous drawings
+
+  fill(iconColorArray[0], iconColorArray[1], iconColorArray[2]);
+  rect(x, y, iconSize, iconSize); //Icon container
+
+  strokeWeight(3);
+  line(x + iconSize * 0.25, y + iconSize * 0.4, x + iconSize * 0.75, y + iconSize * 0.4); // Horizontal line
+  line(x + iconSize * 0.25, y + iconSize * 0.6, x + iconSize * 0.75, y + iconSize * 0.6); // Horizontal line
+};
+
+
+var makeIcon = function(x, y, iconType, RGBArray, relativeSize) {
+  var targetSize = relativeSize * defaultIconSize || defaultIconSize;
+  var targetColor = RGBArray || [200, 200, 200];
+
+  switch (iconType) {
+    case "addition":
+        makeAddIcon(x, y, targetSize, targetColor);
+        break;
+    case "subtraction":
+        makeSubtractionIcon(x, y, targetSize, targetColor);
+        break;
+    case "equals":
+        makeEqualIcon(x, y, targetSize, targetColor);
+        break;
+  }
+};
+
+// makeIcon(width * 0.1, 100, "addition", [100, 254, 16], 1.3 );
+// makeIcon(width * 0.3, 50, "addition");
+// makeIcon(width * 0.6, 50, "subtraction");
+
+var makeCountPanel = function(x, y, relativeSize) {
+  var counter = 0;
+
+  var targetIconSize = defaultIconSize * relativeSize;
+  var targetTextSize = defaultIconSize * relativeSize * 0.9;
+
+  var coordinates = {
+    panel:[[x, y], [x + targetIconSize * 4, y + targetIconSize]]
+  };
+
+  textSize(targetTextSize);
+  fill(0, 0, 0);
+  text(counter, x + targetIconSize * 1.75, y + targetTextSize);
+
+  var subtractionIcon = makeIcon(x, y, "subtraction");
+  coordinates.subtraction = [[x, y],[x + targetIconSize, y + targetIconSize]];
+
+  var additionIcon = makeIcon(x + targetIconSize * 3, y, "addition");
+  coordinates.addition = [[x + targetIconSize * 3, y],[x + targetIconSize * 4, y + targetIconSize]];
+
+
+};
+
+makeCountPanel(100, 100, 1.0);
