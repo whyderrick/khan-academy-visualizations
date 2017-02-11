dfdd7644db715ac8030ca57b62c01d5037b74373
Derrick Carr <derrick.the.future@gmail.com>
2017-02-10 23:45:30 -0800

    Refactor OOJS to actually work
    This commit makes a handful of to probably set up constructor methods
    and related instances.
    
    The placement of the counter value still needs to be corrected, but the
    code now has objects that can be used to monitor mouseclick locations.

--- simple_counter.js
+++ simple_counter.js
@@ -1,153 +1,72 @@
-var defaultIconSize = 25;
-
-var makeAddIcon = function(x, y, iconSize, iconColorArray) {
-  strokeWeight(1); //Reinitialize default from previous drawings
-
-  fill(iconColorArray[0], iconColorArray[1], iconColorArray[2]);
-  rect(x, y, iconSize, iconSize); //Icon container
-
-  strokeWeight(3);
-  line(x + iconSize * 0.25, y + iconSize * 0.5, x + iconSize * 0.75, y + iconSize * 0.5); // Horizontal line
-  line(x + iconSize * 0.5, y + iconSize * 0.25, x + iconSize * 0.5, y + iconSize * 0.75); // Vertical line
-};
-var makeSubtractionIcon = function(x, y, iconSize, iconColorArray) {
-  strokeWeight(1); //Reinitialize default from previous drawings
-
-  fill(iconColorArray[0], iconColorArray[1], iconColorArray[2]);
-  rect(x, y, iconSize, iconSize); //Icon container
-
-  strokeWeight(3);
-  line(x + iconSize * 0.25, y + iconSize * 0.5, x + iconSize * 0.75, y + iconSize * 0.5); // Horizontal line
-};
-var makeEqualIcon = function(x, y, iconSize, iconColorArray) {
-  strokeWeight(1); //Reinitialize default from previous drawings
-
-  fill(iconColorArray[0], iconColorArray[1], iconColorArray[2]);
-  rect(x, y, iconSize, iconSize); //Icon container
-
-  strokeWeight(3);
-  line(x + iconSize * 0.25, y + iconSize * 0.4, x + iconSize * 0.75, y + iconSize * 0.4); // Horizontal line
-  line(x + iconSize * 0.25, y + iconSize * 0.6, x + iconSize * 0.75, y + iconSize * 0.6); // Horizontal line
-};
-
-
-var makeIcon = function(x, y, iconType, RGBArray, relativeSize) {
-  var targetSize = relativeSize * defaultIconSize || defaultIconSize;
-  var targetColor = RGBArray || [200, 200, 200];
-
-  switch (iconType) {
-    case "addition":
-        makeAddIcon(x, y, targetSize, targetColor);
-        break;
-    case "subtraction":
-        makeSubtractionIcon(x, y, targetSize, targetColor);
-        break;
-    case "equals":
-        makeEqualIcon(x, y, targetSize, targetColor);
-        break;
-  }
-};
-
-// makeIcon(width * 0.1, 100, "addition", [100, 254, 16], 1.3 );
-// makeIcon(width * 0.3, 50, "addition");
-// makeIcon(width * 0.6, 50, "subtraction");
-
-var makeCountPanel = function(x, y, relativeSize) {
-  var counter = 0;
-
-  var targetIconSize = defaultIconSize * relativeSize;
-  var targetTextSize = defaultIconSize * relativeSize * 0.9;
-
-  var coordinates = {
-    panel:[[x, y], [x + targetIconSize * 4, y + targetIconSize]]
-  };
-
-  textSize(targetTextSize);
-  fill(0, 0, 0);
-  text(counter, x + targetIconSize * 1.75, y + targetTextSize);
-
-  var subtractionIcon = makeIcon(x, y, "subtraction");
-  coordinates.subtraction = [[x, y],[x + targetIconSize, y + targetIconSize]];
-
-  var additionIcon = makeIcon(x + targetIconSize * 3, y, "addition");
-  coordinates.addition = [[x + targetIconSize * 3, y],[x + targetIconSize * 4, y + targetIconSize]];
-
-
-};
-
-makeCountPanel(100, 100, 1.0);
-
-// Actually here
 var defaultIconSize = 30;
-
+// background(0, 0 , 0);
 var OperationIcon = function(options) {
-  this.xPos = options[x];
-  this.yPos = options[y];
-  this.type = options[type];
-  this.iconColor = options[rgbArray] || [200, 200, 200];
-  this.iconSize = options[relativeSize] * defaultIconSize || defaultIconSize;
-  this.coordinates = [[xPos, yPos],[xPos + this.iconSize, yPos + this.iconSize]]
-}
+  this.x = options.x;
+  this.y = options.y;
+  this.type = options.type;
+  this.iconColorArray = options.rgbArray || [200, 200, 200];
+  this.iconSize = options.relativeSize * defaultIconSize || defaultIconSize;
+  this.coordinates = [[this.xPos, this.yPos],[this.xPos + this.iconSize, this.yPos + this.iconSize]];
+};
 
 OperationIcon.prototype.makeIcon = function(){
   if(this.type === "addition") {
     strokeWeight(1); //Reinitialize default from previous drawings
 
-    fill(iconColorArray[0], iconColorArray[1], iconColorArray[2]);
-    rect(x, y, iconSize, iconSize); //Icon container
+    fill(this.iconColorArray[0], this.iconColorArray[1], this.iconColorArray[2]);
+    rect(this.x, this.y, this.iconSize, this.iconSize); //Icon container
 
     strokeWeight(3);
-    line(x + iconSize * 0.25, y + iconSize * 0.5, x + iconSize * 0.75, y + iconSize * 0.5); // Horizontal line
-    line(x + iconSize * 0.5, y + iconSize * 0.25, x + iconSize * 0.5, y + iconSize * 0.75); // Vertical line
+    line(this.x + this.iconSize * 0.25, this.y + this.iconSize * 0.5, this.x + this.iconSize * 0.75, this.y + this.iconSize * 0.5); // Horizontal line
+    line(this.x + this.iconSize * 0.5, this.y + this.iconSize * 0.25, this.x + this.iconSize * 0.5, this.y + this.iconSize * 0.75); // Vertical line
   }
   if(this.type === "subtraction") {
     strokeWeight(1); //Reinitialize default from previous drawings
 
-    fill(iconColorArray[0], iconColorArray[1], iconColorArray[2]);
-    rect(x, y, iconSize, iconSize); //Icon container
+    fill(this.iconColorArray[0], this.iconColorArray[1], this.iconColorArray[2]);
+    rect(this.x, this.y, this.iconSize, this.iconSize); //Icon container
 
     strokeWeight(3);
-    line(x + iconSize * 0.25, y + iconSize * 0.5, x + iconSize * 0.75, y + iconSize * 0.5); // Horizontal line
+    line(this.x + this.iconSize * 0.25, this.y + this.iconSize * 0.5, this.x + this.iconSize * 0.75, this.y + this.iconSize * 0.5); // Horizontal line
   }
   if(this.type === "equals") {
     strokeWeight(1); //Reinitialize default from previous drawings
 
-    fill(iconColorArray[0], iconColorArray[1], iconColorArray[2]);
-    rect(x, y, iconSize, iconSize); //Icon container
+    fill(this.iconColorArray[0], this.iconColorArray[1], this.iconColorArray[2]);
+    rect(this.x, this.y, this.iconSize, this.iconSize); //Icon container
 
     strokeWeight(3);
-    line(x + iconSize * 0.25, y + iconSize * 0.4, x + iconSize * 0.75, y + iconSize * 0.4); // Horizontal line
-    line(x + iconSize * 0.25, y + iconSize * 0.6, x + iconSize * 0.75, y + iconSize * 0.6); // Horizontal line
+    line(this.x + this.iconSize * 0.25, this.y + this.iconSize * 0.4, this.x + this.iconSize * 0.75, this.y + this.iconSize * 0.4); // Horizontal line
+    line(this.x + this.iconSize * 0.25, this.y + this.iconSize * 0.6, this.x + this.iconSize * 0.75, this.y + this.iconSize * 0.6); // Horizontal line
   }
   else {
-    alert('Invalid icon type. Try "addition", "subtraction", or "equals".')
+    // alert('Invalid icon type. Try "addition", "subtraction", or "equals".');
   }
 };
 
-var CountPanel = function(x, y, relativeSize){
+var CountPanel = function(options){
   this.panelCount = 0;
 
-  this.xPos = options[x];
-  this.yPos = options[y];
-  this.type = options[type];
-  this.iconColor = options[rgbArray] || [200, 200, 200];
-  this.panelColor = options[panelRGBArray] || [255, 255, 255];
-  this.iconSize = options[relativeSize] * defaultIconSize || defaultIconSize;
-  this.coordinates = [[xPos, yPos],[xPos + this.iconSize * 4.2, yPos + this.iconSize]];
+  this.x = options.x;
+  this.y = options.y;
+  this.iconColor = options.rgbArray || [200, 200, 200];
+  this.panelColor = options.panelRGBArray || [255, 255, 255];
+  this.iconSize = options.relativeSize * defaultIconSize || defaultIconSize;
+  this.coordinates = [[this.x, this.y],[this.x + this.iconSize * 4.2, this.y + this.iconSize]];
 
 // Set up the icon options
   var leftIconOptions = {
-    iconColor: this.iconColor;
-    iconSize: this.iconSize;
-    x: this.xPos * 1.1;
-    y: this.yPos;
+    iconColorArray: this.iconColor,
+    iconSize: this.iconSize,
+    x: this.x * 1.1,
+    y: this.y,
   };
 
   var rightIconOptions = {
-    iconColor: this.iconColor;
-    iconSize: this.iconSize;
-    x: this.xPos * 3.1;
-    y: this.yPos;
+    iconColorArray: this.iconColor,
+    iconSize: this.iconSize,
+    x: this.x * 3.1,
+    y: this.y,
   };
 
   // Assign the addition and subtraction iconSize
@@ -156,6 +75,8 @@ var CountPanel = function(x, y, relativeSize){
 
   // Create the panel view
   this.showPanel = function() {
+    var originalCaller = this;
+
     this.leftIcon = new OperationIcon(leftIconOptions);
     this.leftIcon.makeIcon();
 
@@ -163,10 +84,27 @@ var CountPanel = function(x, y, relativeSize){
     this.rightIcon.makeIcon();
 
     this.showCounter = function(){
-      var targetTextSize = this.iconSize * relativeSize * 0.9;
+      fill(255, 0, 0);
+      var targetTextSize = originalCaller.iconSize * 0.8;
+      text(targetTextSize, 200, 200);
       textSize(targetTextSize);
       fill(0, 0, 0);
-      text(counter, xPos + this.iconSize * 1.75, yPos + targetTextSize);
-    }
-  }
+      text(originalCaller.panelCount, originalCaller.x + originalCaller.iconSize * 2.1, originalCaller.y + targetTextSize);
+    };
+    this.showCounter();
+  };
+};
+var panel1options = {
+  x: 50,
+  y: 50,
+};
+
+var panel1 = new CountPanel(panel1options);
+panel1.showPanel();
+// text(panel1.panelCount, 30, 50);
+
+var icon1options = {
+    x: 200,
+    y: 200,
+    type: "equals",
 };
