7c3e3f86aa1c2f63f406555eb869156bb054e044
Derrick Carr <derrick.the.future@gmail.com>
2017-02-13 00:08:41 -0800

    Switch Sign Icon Implemented
    This commit implements the variable operation sign between the two
    counters. It draws it and adds mouseClicked listener.

--- simple_counter.js
+++ simple_counter.js
@@ -1,24 +1,27 @@
 var defaultIconSize = 30;
 
 var primaryPanelOptions = {
-  x: 50,
+  x: 10,
   y: 50,
 };
 
-var primaryOperatorOptions = {
-  x: 300,
-  y: 100,
-  rgbArray: [],
+var switchSignOptions = {
+  relativeSize: 1.5,
+  x: 150,
+  y: 105,
+  type: "addition",
+  rgbArray: [180, 220, 0],
 };
 
 var secondaryPanelOptions = {
-  x: 50,
+  x: 120,
   y: 170,
 };
 
 var equalSignOptions = {
   x: 300,
   y: 220,
+  type: "equals",
   rgbArray: [],
 };
 
@@ -39,6 +42,7 @@ var OperationIcon = function(options) {
   this.type = options.type;
   this.iconColorArray = options.rgbArray || [200, 200, 200];
 };
+
 OperationIcon.prototype = Object.create(BaseBlock.prototype);
 
 OperationIcon.prototype.clicked = function(){
@@ -51,35 +55,43 @@ OperationIcon.prototype.clicked = function(){
 };
 
 OperationIcon.prototype.draw = function(){
-  var caller = this;
+  //Set Defaults for all icons
 
-  var setup = function() {
-    strokeWeight(1); //Reinitialize default from previous drawings
-
-    fill(caller.iconColorArray[0], caller.iconColorArray[1], caller.iconColorArray[2]);
-    rect(caller.x, caller.y, caller.iconSize, caller.iconSize); //Icon container
-
-    strokeWeight(3);
-  };
+  strokeWeight(1); //Reinitialize default from previous drawings
+  fill(this.iconColorArray[0], this.iconColorArray[1], this.iconColorArray[2]);
+  rect(this.x, this.y, this.iconSize, this.iconSize); //Icon container
+  strokeWeight(3);
 
   if(this.type === "addition") {
-    setup(this);
-    line(this.x + this.iconSize * 0.25, this.y + this.iconSize * 0.5, this.x + this.iconSize * 0.75, this.y + this.iconSize * 0.5); // Horizontal line
-    line(this.x + this.iconSize * 0.5, this.y + this.iconSize * 0.25, this.x + this.iconSize * 0.5, this.y + this.iconSize * 0.75); // Vertical line
+    // Horizontal plus line
+    line( this.x + this.iconSize * 0.25, // Starting x
+          this.y + this.iconSize * 0.5, // Starting y
+          this.x + this.iconSize * 0.75, // Ending x
+          this.y + this.iconSize * 0.5  ); // Ending y
+    // Vertical plus line
+    line(this.x + this.iconSize * 0.5, // Starting x
+         this.y + this.iconSize * 0.25, //Starting y
+         this.x + this.iconSize * 0.5, // Ending x
+         this.y + this.iconSize * 0.75  ); // Ending y
   }
-  if(this.type === "subtraction") {
-    setup(this);
-
-    line(this.x + this.iconSize * 0.25, this.y + this.iconSize * 0.5, this.x + this.iconSize * 0.75, this.y + this.iconSize * 0.5); // Horizontal line
+  else if(this.type === "subtraction") {
+    // Horizontal line
+    line(this.x + this.iconSize * 0.25, // Starting x
+         this.y + this.iconSize * 0.5, // Starting y
+         this.x + this.iconSize * 0.75, // Starting x
+         this.y + this.iconSize * 0.5  ); // Starting y
   }
-  if(this.type === "equals") {
-    setup(this);
-
-    line(this.x + this.iconSize * 0.25, this.y + this.iconSize * 0.4, this.x + this.iconSize * 0.75, this.y + this.iconSize * 0.4); // Horizontal line
-    line(this.x + this.iconSize * 0.25, this.y + this.iconSize * 0.6, this.x + this.iconSize * 0.75, this.y + this.iconSize * 0.6); // Horizontal line
-  }
-  else {
-    // alert('Invalid icon type. Try "addition", "subtraction", or "equals".');
+  else if(this.type === "equals") {
+    // Upper Horizontal line
+    line(this.x + this.iconSize * 0.25, //Starting x
+         this.y + this.iconSize * 0.4,  // Starting y
+         this.x + this.iconSize * 0.75, // Ending x
+         this.y + this.iconSize * 0.4  ); // Ending y
+    // Lower horizontal line
+    line(this.x + this.iconSize * 0.25,
+         this.y + this.iconSize * 0.6,
+         this.x + this.iconSize * 0.75,
+         this.y + this.iconSize * 0.6  );
   }
 };
 
@@ -90,13 +102,13 @@ var CountPanel = function(options){
   this.iconColorArray = options.rgbArray || [200, 200, 200];
   this.panelColor = options.panelRGBArray || [255, 255, 255];
 
-  this.xEnd = this.x + this.iconSize * 4.2;
+  this.xEnd = this.iconSize * 4 + this.x;
 
 // Set up the icon options
   var subtractionIconOptions = {
     iconColorArray: this.iconColor,
     iconSize: this.iconSize,
-    x: this.x + (this.x * 1.1),
+    x: this.x,
     y: this.y,
     type: "subtraction",
   };
@@ -104,7 +116,7 @@ var CountPanel = function(options){
   var additionIconOptions = {
     iconColorArray: this.iconColor,
     iconSize: this.iconSize,
-    x: this.x + (this.x * 3.1),
+    x: this.xEnd - this.iconSize,
     y: this.y,
     type: "addition",
   };
@@ -117,17 +129,18 @@ var CountPanel = function(options){
   };
 
   this.showCounter = function() {
-    // Overwrite any counter drawing that already exists
     var targetTextSize = this.iconSize * 0.8;
-
-    var leftBoundary = this.subtractionIcon.xEnd + 1;
-    var rightBoundary = this.additionIcon.xStart - 1;
+    // Define the counter value position within the existing icons.
+    var leftBoundary = this.subtractionIcon.xEnd;
+    var rightBoundary = this.additionIcon.xStart;
     var textXLocation = leftBoundary + (rightBoundary - leftBoundary)/2 - targetTextSize * 0.25;
 
-    noStroke();
+    // Clear the existing counter space before redrawing
+    strokeWeight(1);
     fill(255,255,255);
     rect(leftBoundary + 1, this.yStart, rightBoundary - leftBoundary, this.iconSize);
 
+    // Update the visible counter value
     fill(0,0,0);
     textSize(targetTextSize);
     text(this.panelCount, textXLocation, this.y + targetTextSize);
@@ -141,33 +154,20 @@ var CountPanel = function(options){
 CountPanel.prototype = Object.create(BaseBlock.prototype);
 
 CountPanel.prototype.draw = function() {
+    strokeWeight(1);
     this.subtractionIcon.draw();
     this.additionIcon.draw();
     this.showCounter();
 };
 
-var drawPanel = function(panelObj) {
-  var pOb = panelObj;
-  pOb.subtractionIcon.draw();
-  pOb.additionIcon.draw();
-  pOb.showCounter();
-};
-
-// var drawPanelCounter =  function(panelObj) {
-//   var pOb = panelObj;
-//   var targetTextSize = pOb.iconSize * 0.8;
-//   textSize(targetTextSize);
-//   text(pOb.panelCount, (pOb.additionIcon.xEnd - pOb.xStart) * 0.85, pOb.y + targetTextSize);
-// };
-
-var panel1options = {
-  x: 50,
-  y: 50,
-};
-
 var primaryPanel = new CountPanel(primaryPanelOptions);
-
 primaryPanel.draw();
+
+var switchSign = new OperationIcon(switchSignOptions);
+switchSign.draw();
+
+var secondaryPanel = new CountPanel(secondaryPanelOptions);
+secondaryPanel.draw();
 
 
 var mouseClicked = function() {
@@ -175,11 +175,25 @@ var mouseClicked = function() {
   var pp = primaryPanel;
 
   if(pp.subtractionIcon.clicked()) {
-    pp.panelCount--;
+    // Make sure counter stays in positive numbers as appropriate for the lesson
+    if(pp.panelCount > 0){
+      pp.panelCount--;
+    }
     pp.showCounter();
   }
   if (pp.additionIcon.clicked()) {
     pp.panelCount++;
     pp.showCounter();
+  }
+
+  // Switch Sign
+  if (switchSign.clicked()) {
+    if(switchSign.type === "addition"){
+        switchSign.type = "subtraction";
+        switchSign.draw();
+    } else {
+      switchSign.type = "addition";
+      switchSign.draw();
+    }
   }
 };
