0a5edc574929febe83dd525ca5e9c2e7c890ebe4
Derrick Carr <derrick.the.future@gmail.com>
2017-02-12 22:02:11 -0800

    Working counter update & Protypal Inheritance
    This commit adds functionality to draw over the existing counter space
    to address the issue of overlapping values.
    
    It also establishes a BaseBlock constructor for Icons and Panels to
    inherit attributes from.

--- simple_counter.js
+++ simple_counter.js
@@ -1,41 +1,81 @@
 var defaultIconSize = 30;
-// background(0, 0 , 0);
-var OperationIcon = function(options) {
-  this.x = options.x;
-  this.y = options.y;
-  this.type = options.type;
-  this.iconColorArray = options.rgbArray || [200, 200, 200];
-  this.iconSize = options.relativeSize * defaultIconSize || defaultIconSize;
-  this.coordinates = [[this.x, this.y],[this.x + this.iconSize, this.y + this.iconSize]];
+
+var primaryPanelOptions = {
+  x: 50,
+  y: 50,
 };
 
-OperationIcon.prototype.makeIcon = function(){
-  if(this.type === "addition") {
+var primaryOperatorOptions = {
+  x: 300,
+  y: 100,
+  rgbArray: [],
+};
+
+var secondaryPanelOptions = {
+  x: 50,
+  y: 170,
+};
+
+var equalSignOptions = {
+  x: 300,
+  y: 220,
+  rgbArray: [],
+};
+
+var BaseBlock = function(options) {
+  this.x = options.x;
+  this.y = options.y;
+  this.iconSize = options.relativeSize * defaultIconSize || defaultIconSize;
+  this.coordinates = [[this.x, this.y],
+                      [this.x + this.iconSize, this.y + this.iconSize]];
+  this.xStart = this.x;
+  this.xEnd = this.x + this.iconSize;
+  this.yStart = this.y;
+  this.yEnd = this.y + this.iconSize;
+};
+
+var OperationIcon = function(options) {
+  BaseBlock.call(this, x, y, iconSize, coordinates, xStart, xEnd, yStart, yEnd)
+  this.type = options.type;
+  this.iconColorArray = options.rgbArray || [200, 200, 200];
+};
+
+OperationIcon.prototype = Object.create(BaseBlock.protoype);
+
+OperationIcon.prototype.clicked = function(){
+  if((mouseX >= this.xStart && mouseX <= this.xEnd) &&
+      (mouseY >= this.yStart && mouseY <= this.yEnd)) {
+        return true;
+  } else {
+      return false;
+  }
+};
+
+OperationIcon.prototype.draw = function(){
+  var caller = this;
+
+  var setup = function() {
     strokeWeight(1); //Reinitialize default from previous drawings
 
-    fill(this.iconColorArray[0], this.iconColorArray[1], this.iconColorArray[2]);
-    rect(this.x, this.y, this.iconSize, this.iconSize); //Icon container
+    fill(caller.iconColorArray[0], caller.iconColorArray[1], caller.iconColorArray[2]);
+    rect(caller.x, caller.y, caller.iconSize, caller.iconSize); //Icon container
 
     strokeWeight(3);
+  };
+
+  if(this.type === "addition") {
+    setup(this);
     line(this.x + this.iconSize * 0.25, this.y + this.iconSize * 0.5, this.x + this.iconSize * 0.75, this.y + this.iconSize * 0.5); // Horizontal line
     line(this.x + this.iconSize * 0.5, this.y + this.iconSize * 0.25, this.x + this.iconSize * 0.5, this.y + this.iconSize * 0.75); // Vertical line
   }
   if(this.type === "subtraction") {
-    strokeWeight(1); //Reinitialize default from previous drawings
+    setup(this);
 
-    fill(this.iconColorArray[0], this.iconColorArray[1], this.iconColorArray[2]);
-    rect(this.x, this.y, this.iconSize, this.iconSize); //Icon container
-
-    strokeWeight(3);
     line(this.x + this.iconSize * 0.25, this.y + this.iconSize * 0.5, this.x + this.iconSize * 0.75, this.y + this.iconSize * 0.5); // Horizontal line
   }
   if(this.type === "equals") {
-    strokeWeight(1); //Reinitialize default from previous drawings
+    setup(this);
 
-    fill(this.iconColorArray[0], this.iconColorArray[1], this.iconColorArray[2]);
-    rect(this.x, this.y, this.iconSize, this.iconSize); //Icon container
-
-    strokeWeight(3);
     line(this.x + this.iconSize * 0.25, this.y + this.iconSize * 0.4, this.x + this.iconSize * 0.75, this.y + this.iconSize * 0.4); // Horizontal line
     line(this.x + this.iconSize * 0.25, this.y + this.iconSize * 0.6, this.x + this.iconSize * 0.75, this.y + this.iconSize * 0.6); // Horizontal line
   }
@@ -45,94 +85,106 @@ OperationIcon.prototype.makeIcon = function(){
 };
 
 var CountPanel = function(options){
+  BaseBlock.call(this, x, y, iconSize, coordinates, xStart, yStart, yEnd);
   this.panelCount = 0;
 
-  this.x = options.x;
-  this.y = options.y;
-  this.iconColor = options.rgbArray || [200, 200, 200];
+  this.iconColorArray = options.rgbArray || [200, 200, 200];
   this.panelColor = options.panelRGBArray || [255, 255, 255];
-  this.iconSize = options.relativeSize * defaultIconSize || defaultIconSize;
 
-  this.coordinates = [[this.x, this.y],[this.x + this.iconSize * 4.2, this.y + this.iconSize]];
+  this.xEnd = this.x + this.iconSize * 4.2;
 
 // Set up the icon options
-  var leftIconOptions = {
+  var subtractionIconOptions = {
     iconColorArray: this.iconColor,
     iconSize: this.iconSize,
-    x: this.x * 1.1,
+    x: this.x + (this.x * 1.1),
     y: this.y,
+    type: "subtraction",
   };
 
-  var rightIconOptions = {
+  var additionIconOptions = {
     iconColorArray: this.iconColor,
     iconSize: this.iconSize,
-    x: this.x * 3.1,
+    x: this.x + (this.x * 3.1),
     y: this.y,
+    type: "addition",
   };
 
-  // Assign the addition and subtraction iconSize
-  leftIconOptions.type = "subtraction";
-  rightIconOptions.type = "addition";
+  this.subtractionIcon = new OperationIcon(subtractionIconOptions);
+  this.additionIcon = new OperationIcon(additionIconOptions);
 
-  // Create the panel view
   this.showPanel = function() {
-    var origCal = this;
-
-    this.leftIcon = new OperationIcon(leftIconOptions);
-    this.leftIcon.makeIcon();
-
-    this.rightIcon = new OperationIcon(rightIconOptions);
-    this.rightIcon.makeIcon();
-
-    this.showCounter = function(){
-      fill(255, 0, 0);
-      var targetTextSize = origCal.iconSize * 0.8;
-      textSize(targetTextSize);
-      fill(0, 0, 0);
-      text(origCal.panelCount, (origCal.rightIcon.coordinates[1][0] - origCal.coordinates[0][0]) * 0.85, origCal.y + targetTextSize);
-    };
-    this.showCounter();
+    this.subtractionIcon.makeIcon();
   };
+
+  this.showCounter = function() {
+    // Overwrite any counter drawing that already exists
+    var targetTextSize = this.iconSize * 0.8;
+
+    var leftBoundary = this.subtractionIcon.xEnd + 5;
+    var rightBoundary = this.additionIcon.xStart - 5 - leftBoundary;
+
+    var textXLocation = rightBoundary/2 - targetTextSize;
+
+    fill(255,255,255);
+    rect(leftBoundary + 1, this.yStart, rightBoundary, this.iconSize);
+
+    fill(0,0,0);
+    textSize(targetTextSize);
+    text(this.panelCount, textXLocation, this.y + targetTextSize);
+  };
+
+
+//   Create the panel view
+//   this.showPanel = function() {
+//     this.subtractionIcon.makeIcon();
+//     this.additionIcon.makeIcon();
+
+//     fill(255, 0, 0);
+//     var targetTextSize = this.iconSize * 0.8;
+//     textSize(targetTextSize);
+//     fill(0, 0, 0);
+//     text(this.panelCount, (this.additionIcon.xEnd - this.xStart) * 0.85, this.y + targetTextSize);
+//   };
 };
+
+var drawPanel = function(panelObj) {
+  var pOb = panelObj;
+  pOb.subtractionIcon.draw();
+  pOb.additionIcon.draw();
+  pOb.showCounter();
+};
+
+var drawPanelCounter =  function(panelObj) {
+  var pOb = panelObj;
+  var targetTextSize = pOb.iconSize * 0.8;
+  textSize(targetTextSize);
+  text(pOb.panelCount, (pOb.additionIcon.xEnd - pOb.xStart) * 0.85, pOb.y + targetTextSize);
+};
+
 var panel1options = {
   x: 50,
   y: 50,
 };
 
-var panel1 = new CountPanel(panel1options);
-panel1.showPanel();
+var primaryPanel = new CountPanel(primaryPanelOptions);
 
-var icon1options = {
-  x: 200,
-  y: 200,
-  type: "equals",
+drawPanel(primaryPanel);
+
+var draw = function() {
+//   primaryPanel.showCounter();
 };
 
-text(panel1.leftIcon.coordinates[0][0], 200, 200);
-text(panel1.leftIcon.coordinates[1][0], 300, 200);
-
-// draw = function() {
-//   if (mouseX > panel1.leftIcon.coordinates[0][0] &&
-//       mouseX < panel1.leftIcon.coordinates[1][0]) {
-//       rect(20,20, 20,20);
-//   }
-// };
-
 var mouseClicked = function() {
+  var pp = primaryPanel;
 
-  // panel1
-  var p1xStart = panel1.leftIcon.coordinates[0][0];
-  var p1xEnd = panel1.leftIcon.coordinates[1][0];
-  var p1yStart = panel1.leftIcon.coordinates[0][1];
-  var p1yEnd = panel1.leftIcon.coordinates[1][1];
-
-  if ((mouseX >= p1xStart && mouseX <= p1xEnd) &&
-       (mouseY >=p1yStart && mouseY <= p1yEnd)){
-      rect(20,20, 20,20);
-      panel1.panelCount--;
-  } else if ((mouseX >= panel1.rightIcon.coordinates[0][0] && mouseX <= panel1.rightIcon.coordinates[1][0]) &&
-  (mouseY >= panel1.rightIcon.coordinates[0][1] && mouseY <= panel1.rightIcon.coordinates[1][1])) {
-    panel1.panelCount++;
+  if(pp.subtractionIcon.clicked()) {
+    pp.panelCount--;
+    // rect(clickX, 0, 15, 15);
+    pp.showCounter();
   }
-  text(panel1.panelCount, 100, 300);
+  if (primaryPanel.rightIcon.clicked()) {
+    pp.panelCount++;
+    text("y.", clickX, 130);
+  }
 };
